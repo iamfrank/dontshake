1d60d494bfb019574a3422242e90ef5b17d96899
Frank R Thomsen <iamfrank@users.noreply.github.com>
2017-11-27 21:37:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ var gn = new GyroNorm(),
     ns = new NoSleep(),
     start_btn = document.getElementById('start-btn'),
     test_btn = document.getElementById('test-btn'),
-    horn = new Audio('/audio/air_horn.mp3'),
-    bell = new Audio('/audio/temple_bell.mp3');
-    pin = new Audio('/audio/pin_dropping.mp3'),
+    horn = new Audio('../audio/air_horn.mp3'),
+    bell = new Audio('../audio/temple_bell.mp3'),
+    pin = new Audio('../audio/pin_dropping.mp3'),
     game_on = false;
 
 
