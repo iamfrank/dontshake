bc876c5c42793dbbcc75368eadd7000d3ef19445
iamfrank <frank@magenta.dk>
2018-03-30 21:38:29 +0200

    restyle

--- js/main.js
+++ js/main.js
@@ -1,61 +1,22 @@
 /*
- * Revision 1.6
+ * Revision 1.7
  */ 
 
 // Consider https://github.com/kittykatattack/sound.js
 // or https://github.com/rserota/wad/blob/master/src/wad.js
 
-/*
-var start_btn = document.getElementById('start-btn'),
-    test_btn = document.getElementById('test-btn'),
-    horn = new Audio('./audio/air_horn.mp3'),
-    bell = new Audio('./audio/temple_bell.mp3'),
-    pin = new Audio('./audio/pin_dropping.mp3'),
-    game_on = false;
-
-function gameStart() {
-    bell.play();
-    vm.msg = 'Joust!';
-    vm.btn_msg = 'Stop';
-    game_on = true;
-    setTimeout(gameOn, 2000);
-}
-
-function gameOn() {
-    if (game_on) {
-        pin.play();
-        setTimeout(gameOn, 2000);
-    }
-}
-
-function gameOver() {
-    if (game_on) {
-        horn.play();
-        game_on = false;
-        vm.msg = 'Game over';
-        vm.btn_msg = 'Reset';
-    }
-}
-
-function reset() {
-    vm.msg = 'Press to play';
-    vm.btn_msg = '';
-}
-
-
-*/
 
 // Initialize Vue instance
-
 new Vue({
     el: '#app',
     data: {
-        msg: 'Press to play',
+        msg: '',
+        btn_msg: 'Start',
         mx: 0,
         my: 0,
         mz: 0,
-        btn_msg: '',
-        active_start_btn: false
+        active_start_btn: false,
+        points: 1000
     },
     methods: {
         handleMotionEvent: function(event) {
@@ -79,19 +40,13 @@ new Vue({
                 this.active_start_btn = true;
                 gameStart();
             }
-        },
-        testSound: function () {
-            //bell.play();
         }
     },
     created: function() {
         if (window.DeviceMotionEvent) {
             window.addEventListener('devicemotion', this.handleMotionEvent);
         } else {
-            vm.msg = 'Sorry! Your device does not display device motion.';
+            this.msg = 'Sorry! Your device does not display device motion.';
         };
     }
 });
-
-
-// Add event listeners
