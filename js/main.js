32208a86ab731f5900de97e23994a58a1576b1b0
Frank Thomsen <frtho@sdfi.dk>
2023-08-10 10:04:10 +0200

    update script

--- js/main.js
+++ js/main.js
@@ -2,41 +2,23 @@
 // Consider https://github.com/kittykatattack/sound.js
 // or https://github.com/rserota/wad/blob/master/src/wad.js
 
+if (!DeviceMotionEvent) {
+  alert('The application is not supported by your device')
+}
+
 const a_el = document.querySelector('.meter-x')
 const b_el = document.querySelector('.meter-y')
 const g_el = document.querySelector('.meter-z')
 const startBtn = document.getElementById('startBtn')
 const logEl = document.getElementById('log')
 
-/*
-function requestOrientationPermission() {
-  DeviceOrientationEvent.requestPermission()
-  .then(response => {
-    if (response === 'granted') {
-      window.addEventListener('deviceorientation', (e) => {
-        alert('orientation'  + e.alpha)
-        a_el.innerHTML = e.alpha
-        b_el.innerHTML = e.beta
-        g_el.innerHTML = e.gamma
-      })
-    }
-  })
-  .catch(err => {
-    alert('no device motion')
-    console.error(err)
-  })
-}
-*/
-
 function setMotionListener() {
-
-  window.addEventListener('deviceorientation', (event) => {
+  window.addEventListener('devicemotion', (event) => {
     logEl.innerHTML = 'Got motion data ' + event
     a_el.innerHTML = event.accelerationIncludingGravity.x
     b_el.innerHTML = event.accelerationIncludingGravity.y
     g_el.innerHTML = event.accelerationIncludingGravity.z
   })
-
 }
 
 startBtn.addEventListener('click', (e) => {
